18b8d93f57c715ee9f1800cce06b8ff1a649501c
cbrasantos <cbr.asantos@gmail.com>
2022-04-30 20:06:36 -0300

    Trabalhando com Ansible. Adicionado Ansible-Lab

---
+++ ansible-lab/db01/provision.sh
@@ -0,0 +1,4 @@
+#/bin/sh    
+cat <<EOT >> /home/vagrant/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDBnN/w8ubWemyB6yZvCrf6yHSeptXimMj+zOhSss6J4TWQStoXiIn7KQMZ/4VD581XnGvtTwW6/QVHQSOcxVvf0CYTCI1EtLpih8nCiRN14Zqmx1b9FUYRjvCE+d8YB3Gcy9Dh4LydBoxpRWCXjAePMKBTCy31mjB3ipDc64E9dulOCfQh+5Zxx/YyoBbvm3Bv9U/Udk7Lx0gWCcQR+tLLgX9AHTnBHcAmURxXtp0t8S67/ucQw4QBkCtEDZIu7If4wW+CDLVplRfkJFYYZmGlCplVzrD7GJlG3LJtZ3oDR3obXGkctwGihcy8NrI9LbRH+5hox2UOiZtW8Pn6AEvt vagrant@control-node
+EOT
